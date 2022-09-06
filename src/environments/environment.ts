aeff61f2a941297d3f6d1fd721e6dc4cba5a9fd2
Masixole Matolweni <72172920+Matolweni95@users.noreply.github.com>
2022-09-06 17:06:50 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://newsapi.org/v2/top-headlines?country=za&apiKey=cfe1a5ae77f24bb283a94d475125065b'  
+  baseUrl: 'https://newsapi.org/v2/top-headlines?country=za&apiKey='  
 };
 
 /*
